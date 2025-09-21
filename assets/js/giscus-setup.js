b4079d06993760b60f35ca552789625408b44189
wangqian2149185 <wangqian2149185@users.noreply.github.com>
2025-09-21 01:53:16 +0000

    Deploying to gh-pages from @ wangqian2149185/al-folio@680aa3b89be05c3499b7fa10975eaf9f68b37b4f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "",
+    "data-repo": "wangqian2149185/al-folio",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
